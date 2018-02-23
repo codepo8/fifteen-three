db016a585f69c22d63ed6ff1bcd610806ec62c96
codepo8 <you@example.com>
2018-02-23 01:22:46 +0000

    player controls, local storage

--- fifteen-three.js
+++ fifteen-three.js
@@ -1,49 +1,93 @@
 
 /* DOM ELEMENTS */
-const throwbutton = document.querySelector('#roll');
-const calculation = document.querySelector('#calculation');
-const die1 = document.querySelector('#die1');
-const die2 = document.querySelector('#die2');
-const die3 = document.querySelector('#die3');
-const operators = document.querySelector('#operators');
-const errorfield = document.querySelector('#errorfield');
-const rollresults = document.querySelector('#rollresults');
-const progress = document.querySelector('#progressbar');
-const fullscore = document.querySelector('#tomatch');
+const throwbutton     = document.querySelector('#roll');
+const calculation     = document.querySelector('#calculation');
+const die1            = document.querySelector('#die1');
+const die2            = document.querySelector('#die2');
+const die3            = document.querySelector('#die3');
+const operators       = document.querySelector('#operators');
+const errorfield      = document.querySelector('#errorfield');
+const rollresults     = document.querySelector('#rollresults');
+const progress        = document.querySelector('#progressbar');
+const fullscore       = document.querySelector('#tomatch');
+const playersection   = document.querySelector('#players');
+const addplayerbutton = document.querySelector('#addplayer');
+const playerform      = document.querySelector('#playerlist');
+const playername      = document.querySelector('#playername');
+const restartbutton   = document.querySelector('#newgame');
+const playbutton      = document.querySelector('#playgame');
+const gamescreen      = document.querySelector('#game');
+const introscreen     = document.querySelector('#intro');
+const gameoverscreen  = document.querySelector('#gameover');
+
 let currentterm = '';
 let config = {};
-let edited = null;
-let players = [];
-
-document.querySelector('#players').addEventListener('click', (ev) => {
-    let t = ev.target;
-    if (t.tagName === 'INPUT' && t.type === "button") {
-        t.type = "text";
-        t.select();
-        edited = t;
-    }
-}); 
-document.querySelector('#players form').addEventListener('submit', (ev) => {
-    edited.type = "button";
-    ev.preventDefault();
-}); 
-
-
+let currentplayer = 0;
+let players = localStorage.playercache ?  JSON.parse(localStorage.playercache) : [];
 
 const init = () => {
-    throwbutton.className = 'active';
-    rollresults.className = 'hidden';
-    errorfield.className = 'hidden';
 
     fetch('gameconfig.json').then(function(response) {
-        console.log(response);
         return response.text();
       }).then(function(text) {
         config = JSON.parse(text);
-        console.log(config);
     });
 
+    restartbutton.addEventListener('click', setupgame);
+    throwbutton.addEventListener('click', rollthem);
+    rollresults.addEventListener('click', getdice);
+    operators.addEventListener('click', operatorfunctions);
+    progressbar.addEventListener('animationend' ,outoftime);
+    addplayerbutton.addEventListener('click', toggleplayerform);
+    playerform.addEventListener('submit', addplayer);
+    playerform.querySelector('ul').addEventListener('click', removeplayer);
+    setupgame();
 }
+
+const addplayer = (ev) => {
+    ev.preventDefault();
+    if (playername.value !== '') {
+        console.log(playername.value);
+        players.push(
+            {"name": playername.value, "score": 0}
+        );
+        playername.value = '';
+        playername.classList.remove('show');
+        populateplayers(currentplayer);
+    }
+};
+
+const removeplayer = (ev) => {
+    let t = ev.target;
+    if (t.tagName === "A" && t.parentNode.tagName === 'LI') {
+        let deadplayer = players.splice(t.dataset.num, 1)
+        populateplayers(currentplayer);
+    }
+    ev.preventDefault();
+}
+const toggleplayerform = (ev) => {
+    playername.classList.toggle('show');
+    playername.focus();
+}
+const populateplayers = (turn) => {
+    localStorage.playercache = JSON.stringify(players);
+    playerform.querySelector('ul').innerHTML = '';
+    players.forEach((p, k) => {
+        let list =  document.querySelector('#playerlist ul');
+        let item = document.createElement('li');
+        if (k === turn) {
+            item.classList.add('currentplayer');
+        }
+        item.dataset.num = k;
+        item.dataset.score = 0;
+        item.innerHTML= `
+            ${p.name}
+            <span>${p.score}</span>
+            <a data-num="${k}"href="#">x</a>
+        `;
+        list.appendChild(item);
+    });
+};
 
 const getdice = (ev) => {
     let t = ev.target;
@@ -60,7 +104,7 @@ const operatorfunctions = (ev) => {
     let t = ev.target;
     if (t.tagName !== 'BUTTON') {return;}
     if (t.dataset.val === 'delete') {
-        clearresult();
+        clearmove();
         return;
     } 
     if (t.dataset.val === 'done') {
@@ -89,18 +133,48 @@ const operatorfunctions = (ev) => {
             errorfield.className = '';
         } 
         if (finalvalue >= 0 && finalvalue < 16) {
-            calculation.innerHTML = currentterm + ' = ' + eval(currentterm);
+            calculation.innerHTML = currentterm + ' = ' + finalvalue;
+            if (fullscore.dataset.bestvalue > finalvalue) {
+                if (players.length > 0) {
+                    players[currentplayer].score += fullscore.dataset.bestvalue - finalvalue;
+                }
+            }
+            advanceplayers();
             rollresults.className = 'hidden';
             calculation.classList.remove('error');
             errorfield.className = 'hidden';
             throwbutton.className = ''; 
             progressbar.classList.remove('animated');
+    
         }
         return;
-    } 
-    currentterm += t.dataset.val;
+    }
+    currentterm += ' ' + t.dataset.val + ' ';
     calculation.innerHTML = currentterm;
     ev.preventDefault();
+}
+
+const advanceplayers = () => {
+    if (players.length > 0) {
+        if (players[currentplayer].score >= config.gameendscore) {
+            gameover();
+        } else {
+            currentplayer = (currentplayer + 1) % players.length;
+            populateplayers(currentplayer);
+       }
+    }
+}
+const gameover = () => {
+    setsection(gameoverscreen);
+    let playerscore = players.slice(0);
+    playerscore.sort((a,b) => {
+        return a.score > b.score
+    });
+    let out = '';
+    playerscore.forEach((p) => {
+        out += `<li>${p.name}: ${p.score}</li>`;
+    });
+    gameoverscreen.querySelector('ol').innerHTML = out;
 }
 
 const rollthem = (ev) => {
@@ -111,67 +185,70 @@ const rollthem = (ev) => {
     let valuematch = [valueone, valuetwo, valuethree].sort();
     fullscore.innerHTML = config.winnerpatterns[valuematch.join('')][0];
     fullscore.dataset.bestvalue = config.winnerpatterns[valuematch.join('')][0];
-    // console.log(winnerpatterns[valuematch.join('')][1]);
     die1.title = die1.dataset.val = valueone;
     die2.title = die2.dataset.val = valuetwo;
     die3.title = die3.dataset.val = valuethree;
     die1.className = `dice dice-${valueone}`;
     die2.className = `dice dice-${valuetwo}`;
     die3.className = `dice dice-${valuethree}`;
-    throwbutton.className = 'throwactive';
+    throwbutton.className = 'hidden';
     rollresults.className = '';
     operators.className = '';
+    calculation.innerHTML = '';
+    errorfield.className = 'hidden';
+    currentterm = '';
     ev.preventDefault();
 };
 
 const outoftime = () => {
     operators.className = 'hidden';
+    progress.className = '';
+    rollresults.className = 'hidden';
     errorfield.innerHTML = config.errormessages.outoftime;
     errorfield.className = '';
+    throwbutton.className = '';
+    if (players.length > 0) {
+        players[currentplayer].score += +fullscore.dataset.bestvalue;
+    }
+    advanceplayers();
 }
 
 /* HELPER FUNCTIONS */
 
-const templatereplace = (id, content) => {
-    let newcontent = '';
-    if (document.querySelector(id + ' .temporary')) {
-        document.querySelector(id + ' .temporary').remove();
-    };
-    let t = document.querySelector(id + ' template');
- 
-    if (typeof content === 'object') {
-        newcontent = t.innerHTML;
-        Object.keys(content).forEach((key) => {
-            let pattern = new RegExp('\\$' + key, 'g');
-            newcontent = newcontent.replace(pattern, content[key]);
-        });
-    } else {
-        newcontent = content;
-    }
-    let dummy = document.createElement('div');
-    dummy.className = 'temporary';
-    dummy.innerHTML = newcontent;
-    t.parentNode.insertBefore(dummy, t);
-}
-
-const showmessage = (message, func) => {
-    document.querySelector('#message div').innerHTML = message;
-    document.querySelector('#message').className = '';
-    document.querySelector('#message button').addEventListener('click', (ev) => {
-        document.querySelector('#message').className = 'hidden';
-        if (func) {func()}
-    });
-}
-
-const clearresult = () => {
+ const clearmove = () => {
     errorfield.innerHTML = '';
-    errorfield.className = 'hidden';
-    currentterm = '';
     calculation.innerHTML = '';
-    calculation.className = '';
+    currentterm = '';
+    errorfield.classList.add('hidden');
+    calculation.classList.remove('error');
     die1.classList.remove('selected');
     die2.classList.remove('selected');
     die3.classList.remove('selected');
+ }
+
+const setsection = (stateid) => {
+    [gameoverscreen, gamescreen, introscreen].forEach(s => {
+        s.classList.add('hidden');
+    })
+    stateid.classList.remove('hidden');
+}
+const setupgame = (ev) => {
+    players.forEach(p => {
+        p.score = 0;
+    });
+    currentplayer = 0;
+    currentterm = '';
+    populateplayers(currentplayer);
+    [throwbutton, operators, calculation, playersection].forEach(s => {
+        s.classList.remove('hidden');
+    })
+    calculation.classList.remove('error');
+    errorfield.classList.add('hidden');
+    rollresults.classList.add('hidden');
+    errorfield.innerHTML = '';
+    calculation.innerHTML = '';
+    setsection(gamescreen);
+    if (ev) {ev.preventDefault()}
 };
 
 const throwdice = () => {
@@ -179,9 +256,5 @@ const throwdice = () => {
 }
 
 /* EVENT HANDLERS */
-progressbar.addEventListener('animationend' ,outoftime);
-operators.addEventListener('click' ,operatorfunctions);
-rollresults.addEventListener('click', getdice);
-throwbutton.addEventListener('click',rollthem);
 window.addEventListener('DOMContentLoaded', init);
 
